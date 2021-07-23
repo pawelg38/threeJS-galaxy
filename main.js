1d00a96c1b69b9f4c4970e6052c92d8c396c388c
deseth38 <deseth83@gmail.com>
2021-07-23 13:08:11 +0200

    Add mesh and main loop (view refreshing)

--- main.js
+++ main.js
@@ -15,3 +15,23 @@ const pointLight = new THREE.PointLight(0Xffffff);
 pointLight.position.set(0,0,0);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
+
+
+const galaxyTexture = new THREE.TextureLoader().load('textures/bg.jpg');
+const coneGeometry = new THREE.ConeGeometry( 5, 20, 1000 );
+const coneMaterial = new THREE.MeshStandardMaterial({side: THREE.BackSide, map: galaxyTexture});
+coneMaterial.map.wrapS = THREE.RepeatWrapping;
+coneMaterial.map.wrapT = THREE.RepeatWrapping;
+coneMaterial.map.repeat.set(2, 1);
+const coneMesh = new THREE.Mesh(coneGeometry, coneMaterial);
+coneMesh.rotateX(Math.PI * 1.5);
+coneMesh.position.z -= 2;
+scene.add(coneMesh);
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
